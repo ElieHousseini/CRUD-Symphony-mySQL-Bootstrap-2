d2df27c73b62e21f1e92284aa8d7bdcfb42364ad
Elie Housseini <eliashousseini@gmail.com>
2020-09-27 10:39:16 +0300

    decline registering same email address multiple times in database

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,18 +1,30 @@
 <?php
+/* 
+Main goal to implement UserInterface is so Symfony knows that
+this class is a real user class. Therefore, I can hash it's password
+in the security controller.
+*/
 
 namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+
 // info about user interface: https://bit.ly/2GaawDN
 use Symfony\Component\Security\Core\User\UserInterface;
+
 // info about validation: https://bit.ly/3cCpIFy
 use Symfony\Component\Validator\Constraints as Assert;
 
-
+// info about UniqueEntity: https://bit.ly/3j64NNS
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ * fields = {"email"},
+ * message = "l'email que vous avez indique est deja utitlise !"
+ * )
  */
 class User implements UserInterface
 {
@@ -25,6 +37,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
